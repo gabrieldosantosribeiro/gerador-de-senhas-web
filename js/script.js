fa8c8f53533dd249ac1fee09942ca8f9efb98d69
Gabriel dos Santos <ogabrielmclmg@gmail.com>
2025-12-03 00:18:35 -0300

    up

--- js/script.js
+++ js/script.js
@@ -3,47 +3,138 @@ const minusculas = 'abcdefghijklmnopqrstuvwxyz'
 const numeros = '0123456789'
 const simbolos = '!@#$%&*'
 
-let senha = ''
+let chars = ''
 
-function atualizarForca() {
-    // pegando a barra de força
-    let forca = document.querySelector('.forca')
+// // ------------------ FORÇA DA SENHA ------------------
+// valida o tamanho da senha (4 < x < 20)
+function validarTamanho() {
+    const input = document.querySelector('#tamanho')
 
-    // pegar quais checkbox estao ativas
-    let selecionados = document.querySelectorAll('input[type="checkbox"]:checked').length;
+    const valor = Number(input.value)
+    
+    if (isNaN(valor) || valor < 4 || valor > 20) {
+        input.classList.add('erro')
+        return false
+    } else {
+        input.classList.remove('erro')
+    }
 
-    // alterar o width de acordo com a quantidade
-    forca.style.width = `${selecionados * 25}%`
+    return valor
 }
 
+// pegar todos os iunputs checks e ver quais estão selecionados
+function verificarChecks() {
+    // pegando todos os inputs do tipo checkbox
+    const chekcs = document.querySelectorAll('.opcao input[type="checkbox"]')
+
+    // criando a variável quantidade (vai ser quantos estão ativados)
+    let quantidade = 0
+
+    // para cada input tipo check verificar se está ativado
+    chekcs.forEach(c => {
+        if (c.checked) {
+            quantidade++
+        }
+    })
+
+    if (quantidade == 0) {
+        return false
+    }
+
+    // aqui a quantidade ja é quantos estão ativados
+    return quantidade
+}
+
+// calcular força
+function calcularForca(checks, tam) {
+    // calculando a força
+    const forcaChecks = (checks * 17)
+    const forcaTam = ((tam - 4) * 2)
+
+    const forca = forcaChecks + forcaTam
+
+    return forca
+}
+
+// atualiza a barra de força
+function atualizarForca() {
+    // pegar a barra de força
+    const forca = document.querySelector('.forca')
+
+    // pegar quantos checks estão ativos
+    const checks = verificarChecks() == false ? 0 : verificarChecks()
+
+    // pegar o tamanho da senha
+    const tam = validarTamanho() == false ? 4 : validarTamanho()
+
+    // pegando o valor da força
+    const valorForca = calcularForca(checks, tam)
+
+    // atualizando o tamanho da força
+    forca.style.width = valorForca + '%'
+
+    // atualizando a cor da barra de força
+    atualizarCor(valorForca)
+
+    console.log(forca.style.width)
+}
+
+// ajustar cor da barra de força
+function atualizarCor(width) {
+    // pegar a barra de força
+    const forca = document.querySelector('.forca')
+
+    if (width < 40) {
+        forca.style.background = 'var(--vermelho)'
+    } else if ( width < 80 ) {
+        forca.style.background = 'var(--amarelo)'
+    } else {
+        forca.style.background = 'var(--verde)'
+    }
+}
+
+// função para iniciar a barra de força
+function iniciarForca() {
+    const forca = document.querySelector('.forca')
+    forca.style.width = '0%'
+}
+iniciarForca()
+
+
+// ------------------ SENHA ------------------
+// função para incluir maiusculas na senha
 function incluirMaiusculas() {
     const incluir = document.querySelector('#maiusculas')
     if (incluir.checked) {
-        return senha += maiusculas
+        return chars += maiusculas
     }
 }
 
+// função para incluir minusculas na senha
 function incluirMinusculas() {
     const incluir = document.querySelector('#minusculas')
     if (incluir.checked) {
-        return senha += minusculas
+        return chars += minusculas
     } 
 }
 
+// função para incluir numeros na senha
 function incluirNumeros() {
     const incluir = document.querySelector('#numeros')
     if (incluir.checked) {
-        return senha += numeros
+        return chars += numeros
     }
 }
 
+// função para incluir simbolos na senha
 function incluirSimbolos() {
     const incluir = document.querySelector('#simbolos')
     if (incluir.checked) {
-        return senha += simbolos
+        return chars += simbolos
     }
 }
 
+// função para incluir todas as caracteristicas da senha
 function incluir() {
     incluirMaiusculas()
     incluirMinusculas()
@@ -51,15 +142,67 @@ function incluir() {
     incluirSimbolos()
 }
 
+
+// REFAZER: mesmo com o tamanho da senha inválido gera a senha do tamanho que está no input
+// função que gera a senha
 function gerarSenha() {
     const tamanho = document.querySelector('#tamanho').value
-    incluir()
-    console.log(senha)
-    senha = ''
+
+    if (validarTamanho(tamanho) && verificarChecks() != false) {
+        incluir()
+        const senha = chance.string({
+            length: tamanho,
+            pool: chars
+        })
+
+        chars = ''
+        console.log(senha)
+        mostrarSenha(senha)
+        return senha
+    } else {
+        abrirPopup()
+    }
 }
 
-document.querySelectorAll('.switch').forEach(el => {
-    el.addEventListener('click', () => {
-        atualizarForca()
-    })
-})
+function mostrarSenha(senhaGerada) {
+    campoSenha = document.querySelector('#senha-criada')
+    campoSenha.classList.add('habilitada')
+    senha = document.querySelector('.senha')
+    senha.textContent = senhaGerada
+}
+
+function copiarSenha() {
+    senha = document.querySelector('.senha')
+    icone = document.querySelector('.icone')
+
+    navigator.clipboard.writeText(senha.textContent)
+        .then(() => {
+            icone.name = 'checkmark-outline'
+
+            setTimeout(() => {
+                icone.name = 'copy-outline'
+            }, 1000)
+        })
+    
+}
+
+function abrirPopup() {
+    const tamanho = document.querySelector('#tamanho').value
+    if (!validarTamanho(tamanho)) {
+        document.querySelector('.popup-content p').textContent = 'O tamanho da senha deve ser maior que 4 e menor que 20.'
+        document.querySelector('#popup').style.display = 'flex'
+    } else {
+        document.querySelector('.popup-content p').textContent = 'Pelo menos uma opção de senha deve estar acionada.'
+        document.querySelector('#popup').style.display = 'flex'
+    }
+}
+
+function fecharPopup() {
+    document.querySelector('#popup').style.display = 'none'
+}
+
+// document.querySelectorAll('.switch').forEach(el => {
+//     el.addEventListener('click', () => {
+//         atualizarForca()
+//     })
+// })
